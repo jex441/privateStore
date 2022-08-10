44f33101d9acfc893eb684f969d3af7bb65e93c8
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-10 10:10:15 -0400

    ssl true db

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,10 @@ const dbUrl = process.env.DB_URL || "postgres://localhost:5432/privatestore";
 
 const db = new Sequelize(dbUrl, {
   logging: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
 });
 
 module.exports = db;
