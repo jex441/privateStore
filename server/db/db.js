88b3fa61af36e7bb9ff3a9a23bacd771d9ba9d05
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-10 10:23:18 -0400

    ssl db

--- server/db/db.js
+++ server/db/db.js
@@ -2,12 +2,17 @@ const { Sequelize } = require("sequelize");
 
 const dbUrl = process.env.DB_URL || "postgres://localhost:5432/privatestore";
 
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+if (process.env.NODE_ENV === "production") {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
 const db = new Sequelize(dbUrl, {
   logging: false,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: true,
-  },
 });
 
 module.exports = db;
