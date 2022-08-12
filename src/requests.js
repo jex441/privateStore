51cfb67a2e359feed3eca0bd18aa684b26f3c245
Jeffrey Wood <jwfirenze@gmail.com>
2022-08-12 11:27:33 -0400

    env

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,8 @@
 const superagent = require("superagent");
 require("dotenv").config();
 
-const port = process.env.PORT || "http://localhost:4002";
+// const port = process.env.PORT || "http://localhost:4002";
+const port = "https://privatestore.herokuapp.com";
 
 var root = {
   postCreateUser: async (
